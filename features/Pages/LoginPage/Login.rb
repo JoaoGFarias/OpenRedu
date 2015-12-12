f76fd1841bb32dbe9cefa1e72c0a7e5e0ec2057a
João Guilherme Farias Duda <jgfd@cin.ufpe.br>
2015-12-12 16:16:20 -0300

    Atualizando padrão de nomeação

--- features/Pages/LoginPage/Login.rb
+++ features/Pages/LoginPage/Login.rb
@@ -2,6 +2,6 @@ class Login < SitePrism::Page
   set_url "/"
 
   #Sections
-  section :header, Header_Not_Logged, "div.header-not-logged-in"
+  section :header, HeaderNotLogged, "div.header-not-logged-in"
 
 end
