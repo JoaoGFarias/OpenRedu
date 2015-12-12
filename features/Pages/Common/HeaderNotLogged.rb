1a4c5d04a9bd0dce52ebfb1d9e9080c9a09f6343
João Guilherme Farias Duda <jgfd@cin.ufpe.br>
2015-12-12 17:06:33 -0300

    Page Objects -> @login @sc01

--- features/Pages/Common/HeaderNotLogged.rb
+++ features/Pages/Common/HeaderNotLogged.rb
@@ -1,9 +1,24 @@
-class Header_Not_Logged < SitePrism::Section
+class HeaderNotLogged < SitePrism::Section
 
   #Elements
   element :button_sign_in     , "button#button-sign-in"
   element :username_login     , "input#user_session_login"
   element :password_login     , "input#user_session_password"
   element :button_submit_login, "div.header-sign-in-form-submit input.pull-right"
+  element :opened_login_box   , "div.button-group.dropdown.pull-right.open"
 
+  def log_with_username(username, password)
+
+    if(not is_login_box_opened) then
+      button_sign_in.click
+    end
+
+    username_login.set(username)
+    password_login.set(password)
+
+  end
+
+  def is_login_box_opened
+    return has_opened_login_box? && opened_login_box.visible?
+  end
 end
---
+++ features/Pages/HomePage/Home.rb
@@ -0,0 +1,4 @@
+class Home < SitePrism::Page
+  set_url "/pessoas/{/username}/home"
+
+end
