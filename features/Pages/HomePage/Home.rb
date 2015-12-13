c2d9f1998f5302c9425396e057157b76aa07e201
João Guilherme Farias Duda <jgfd@cin.ufpe.br>
2015-12-13 19:12:27 -0300

    Adicionando espera por carregamento da página

--- features/Pages/HomePage/Home.rb
+++ features/Pages/HomePage/Home.rb
@@ -1,4 +1,18 @@
 class Home < SitePrism::Page
   set_url "/pessoas/{/username}/home"
 
+  #Elements
+  element :welcome_message , "span.home-hello-user"
+
+  def load(username)
+    self.wait_load
+    super(username: username[:username])
+  end
+
+  def wait_load
+    HoldOn.until(timeout: 30, interval: 0.5, message: "Waiting for Home Page") do
+      has_welcome_message? && welcome_message.visible?
+    end
+  end
+
 end
